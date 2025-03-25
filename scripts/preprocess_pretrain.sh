41535df2fa183df8ad383c9a8be2f91719e910e6
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2025-03-25 21:06:48 +0800

    delete

--- scripts/preprocess_pretrain.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python preprocess.py \
---video-path dataset/pretrain_video \
---output-dir dataset/pretrain_video_feature
--- scripts/preprocess_sft.sh
+++
@@ -1,3 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python preprocess.py \
---video-path dataset/sft_video \
---output-dir dataset/sft_video_feature
