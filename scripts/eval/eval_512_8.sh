5f870eb849af979158206e3b2ea1bab26d63ae91
forXuyx <xuyuxiang22@mails.ucas.ac.cn>
2025-04-11 14:47:31 +0800

    update

--- scripts/eval/eval_512_8.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-CUDA_VISIBLE_DEVICES=1 python eval.py \
-    --dim 512 \
-    --n_layers 8 \
-    --use_multi 1 \
-    --model_mode 0 \
--- scripts/eval/eval_768_16.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-CUDA_VISIBLE_DEVICES=1 python eval.py \
-    --dim 768 \
-    --n_layers 16 \
-    --use_multi 1 \
-    --model_mode 0 \
